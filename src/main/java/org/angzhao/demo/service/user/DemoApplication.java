fb83a0dd35980e17f47baa5f542ad110e73f0ce8
wuaz <wuangzhao@live.com>
2020-03-06 07:32:50 +0800

    init()

---
+++ src/main/java/org/angzhao/demo/service/user/DemoApplication.java
@@ -0,0 +1,14 @@
+package org.angzhao.demo.service.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
