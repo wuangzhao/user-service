3038262fb164f8fe3c5f7363580ad18748b5121f
wuaz <wuaz@genvict.com>
2020-03-09 09:36:12 +0800

    feat(前后调通)

---
+++ src/main/java/org/angzhao/demo/service/user/config/CorsConfiguration.java
@@ -0,0 +1,28 @@
+package org.angzhao.demo.service.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author genvict-wuaz
+ * @date 2020/3/9 9:00
+ */
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/web/**")
+                        .allowedOrigins("*")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
